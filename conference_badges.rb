1756a103eb9c016a9b24f317bc2fc157c80b5b0c
Molly McCarron <mccarronmollye@gmail.com>
2018-11-06 12:26:29 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -5,7 +5,7 @@ def badge_maker(name)
 end
 
 def batch_badge_creator(attendees)
-  attendees.map do |attendee|
-    "Hello, my name is #{attendee}."
+  attendees.map do |name|
+    "Hello, my name is #{name}."
 end
 end
