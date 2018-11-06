de42cb6121aa463105b1c00d6a04aed6c0190b92
Molly McCarron <mccarronmollye@gmail.com>
2018-11-06 12:36:13 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -9,3 +9,9 @@ def batch_badge_creator(attendees)
   badge_maker(name)
 end
 end
+
+def assign_rooms(attendees)
+  attendees.each_with_index.map do |name, index|
+    "Hello, #{name}! You'll be assigned to room #{index + 1}!"
+  end
+end
