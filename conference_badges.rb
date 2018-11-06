2f4140b1b4952753f8efcd7deca690fa35c15c2c
Molly McCarron <mccarronmollye@gmail.com>
2018-11-06 12:42:43 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -17,6 +17,11 @@ def assign_rooms(attendees)
 end
 
 def printer
-  puts batch_badge_creator(attendees)
-  puts assign_rooms(attendees)
+  batch_badge_creator(attendees).each do |badge|
+    puts badge
+  end
+  
+  assign_rooms(attendees).each do |assignment|
+    puts assignment
+end
 end
